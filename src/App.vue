79be038313df4dfae1d021b02bbd775864b007b8
NickAstley <andreanicastro1994@gmail.com>
2022-06-24 18:44:26 +0200

    Added album cards generated with an axios request

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Header -->
+    <header class="p-3">
+      <img class="h-100" src="./assets/img/logo-small.svg" alt="">
+    </header>
+    <!-- Main content -->
+    <main>
+      <div class="container h-100">
+        <div class="row h-100 align-items-center">
+          <div class="row row-cols-5 gx-5 gy-3">
+            <AlbumCard v-for="album in albumsList" :key="album.title" 
+            :album-cover-url="album.poster" 
+            :album-title="album.title" 
+            :album-author="album.author" 
+            :album-release-year="album.year"/>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AlbumCard from './components/AlbumCard.vue'
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AlbumCard
+  },
+  data() {
+    return {
+      albumsList: []
+    }
+  },
+  methods: {
+    fetchAlbumsList() {
+      axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((response) => {
+          this.albumsList = response.data.response;
+        });
+    }
+  },
+  mounted() {
+    this.fetchAlbumsList();
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  background-color: #1E2D3B;
+
+  header {
+    height: 7vh;
+    background-color: #2E3A46;
+  }
+  main {
+    height: 93vh;
+  }
 }
 </style>
---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <div class="album-card p-4 text-center h-100">
+        <img class="w-100 mb-3" :src="albumCoverUrl" :alt="albumTitle">
+        <h4 class="text-white text-uppercase mb-3">{{ albumTitle }}</h4>
+        <div class="album-author">{{ albumAuthor }}</div>
+        <span>{{ albumReleaseYear }}</span>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        albumCoverUrl: String,
+        albumTitle: String,
+        albumAuthor: String,
+        albumReleaseYear: String
+    }
+}
+</script>
+
+<style lang="scss">
+.album-card {
+    background-color: #2E3A46;
+    color: #6E6D6E;
+
+    .album-author {
+        font-size: 1.2rem;
+    }
+}
+</style>
