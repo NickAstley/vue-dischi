a18322cc960a75048d81281ac5251b3b03167e02
NickAstley <andreanicastro1994@gmail.com>
2022-06-27 17:37:34 +0200

    Refactored AlbumList into its own component

--- src/components/AlbumCard.vue
+++ src/components/AlbumCard.vue
@@ -1,29 +1,29 @@
 <template>
-<div>
-    <div class="album-card p-4 text-center h-100">
-        <img class="w-100 mb-3" :src="album.poster" :alt="album.title">
-        <h4 class="text-white text-uppercase mb-3">{{ album.title }}</h4>
-        <div class="album-author">{{ album.author }}</div>
-        <span>{{ album.year }}</span>
+    <div>
+        <div class="album-card p-4 text-center h-100">
+            <img class="w-100 mb-3" :src="album.poster" :alt="album.title">
+            <h4 class="text-white text-uppercase mb-3">{{ album.title }}</h4>
+            <div class="album-author">{{ album.author }}</div>
+            <span>{{ album.year }}</span>
+        </div>
     </div>
-</div>
 </template>
 
 <script>
-export default {
-    props: {
-        album: Object
+    export default {
+        props: {
+            album: Object
+        }
     }
-}
 </script>
 
 <style lang="scss">
-.album-card {
-    background-color: #2E3A46;
-    color: #6E6D6E;
+    .album-card {
+        background-color: #2E3A46;
+        color: #6E6D6E;
 
-    .album-author {
-        font-size: 1.2rem;
+        .album-author {
+            font-size: 1.2rem;
+        }
     }
-}
 </style>
---
+++ src/components/AlbumsList.vue
@@ -0,0 +1,65 @@
+<template>
+    <main class="flex-grow-1">
+      <div class="container h-100">
+        <div class="row h-100 align-items-center">
+          <div class="row row-cols-5 gx-5 gy-3">
+            <AlbumCard v-for="album in filteredAlbums" :key="album.title" :album="album"/>
+          </div>
+        </div>
+      </div>
+    </main>
+</template>
+
+<script>
+    import AlbumCard from './AlbumCard';
+    import axios from "axios";
+
+    export default {
+        components: {
+            AlbumCard
+        },
+
+        props: {
+            selectedGenre: String
+        },
+
+        data() {
+            return {
+                albumsList: []
+            }
+        },
+
+        computed: {      
+            getGenresList() {
+                const genresList = [];
+                this.albumsList.forEach((album) =>{
+                    if(!genresList.includes(album.genre)) {
+                        genresList.push(album.genre);
+                    }
+                });
+                return genresList;
+            },
+            filteredAlbums() {
+                if(!this.selectedGenre) {
+                    return this.albumsList;
+                }
+                return this.albumsList.filter((album) =>{
+                    return album.genre === this.selectedGenre;
+                });
+            }
+        },
+
+        methods: {
+            fetchAlbumsList() {
+                axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((response) => {
+                    this.albumsList = response.data.response;
+                    this.$emit("onGenresListCreation", this.getGenresList);
+                });
+            }
+        },
+
+        mounted() {
+            this.fetchAlbumsList();
+        }
+    }
+</script>
